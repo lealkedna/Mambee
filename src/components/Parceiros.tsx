66b21a828f0d057d6aa8a8a77f5ece7d7247b784
EmilenyRochaLeal <emilenyleal20@gmail.com>
2025-01-02 22:14:13 -0300

    style: ajuste de estilo na parte de Parceiros

--- src/components/Parceiros.tsx
+++ src/components/Parceiros.tsx
@@ -9,27 +9,23 @@ function Parceiros(){
             </header>
             <aside className={styles.aside}>
                 <p className={styles.text}>
-                    Na Mambee, nossa escola-fábrica de software, trabalhamos junto 
-                    aos parceiros para criar um ambiente de aprendizado prático, que 
-                    simula os desafios reais do mercado. Esse apoio é essencial para 
-                    estimular a criatividade dos alunos e manter um fluxo contínuo de 
-                    projetos inovadores. Assim, preparamos nossos futuros profissionais 
-                    para o mercado de tecnologia, garantindo que saiam 
-                    com as habilidades e experiências necessárias para enfrentar desafios da 
+                    Na Mambee, nossa escola-fábrica de software, trabalhamos junto<br/> 
+                    aos parceiros para criar um ambiente de aprendizado prático, que <br/>
+                    simula os desafios reais do mercado. Esse apoio é essencial para <br/>
+                    estimular a criatividade dos alunos e manter um fluxo contínuo de <br/>
+                    projetos inovadores. Assim, preparamos nossos futuros profissionais <br/>
+                    para o mercado de tecnologia, garantindo que saiam <br/>
+                    com as habilidades e experiências necessárias para enfrentar desafios da <br/>
                     indústria de forma inovadora e eficiente.
                 </p>
+                <Image 
+                    src="/images/parceiros/Favo de mel Parceiros.svg"
+                    alt="Favos dos parceiros"
+                    width={1120}
+                    height={710}
+                    className={styles.image}
+                />
             </aside>
-            <section className={styles.card}>
-            <div className={styles.hex_grid}>
-          {/* Hexágonos */}
-                <div className={`${styles.hex} ${styles.virtex}`}></div>
-                <div className={`${styles.hex} ${styles.labitec}`}></div>
-                <div className={`${styles.hex} ${styles.petetech}`}></div>
-                <div className={`${styles.hex} ${styles.valedomel}`}></div>
-                <div className={`${styles.hex} ${styles.ifpi}`}></div>
-                <div className={`${styles.hex} ${styles.ibict}`}></div>
-            </div>
-            </section>
         </div>
     );
 }
