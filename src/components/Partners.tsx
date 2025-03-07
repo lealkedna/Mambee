1654f0be6c569f6feda6e49ce582e05a38d6f19e
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2025-03-07 15:31:49 -0300

    atualizando parceiros

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,29 +1,6 @@
-"use client"
 import Image from "next/image";
-import Title1 from "./text/Title1";
-import Hexagon from "./layout/Hexagon";
-import { useEffect, useState } from "react";
 
 export default function Partners() {
-  const [hexSize, setHexSize] = useState(150);
-
-  useEffect(() => {
-    const updateSize = () => {
-      if (window.innerWidth < 640) {
-        setHexSize(100);
-      } else if (window.innerWidth < 1024) {
-        setHexSize(130);
-      } else {
-        setHexSize(150);
-      }
-    };
-
-    updateSize(); // Define o tamanho inicial
-    window.addEventListener("resize", updateSize); // Atualiza ao redimensionar
-
-    return () => window.removeEventListener("resize", updateSize); // Cleanup
-  }, []);
-
   const partners = [
     {
       image: "/images/parceiros/mb.svg",
@@ -68,6 +45,13 @@ export default function Partners() {
       padding: "p-1"
     },
     {
+      image: "/images/parceiros/guaribasvalley.png",
+      nome: "Guaribas Valley",
+      link: "#",
+      color: "#0E4758",
+      padding: "p-1"
+    },
+    {
       image: "/images/parceiros/valedomel.png",
       nome: "Vale do Mel",
       link: "#",
@@ -78,34 +62,20 @@ export default function Partners() {
 
 
   return (
-    <div className="w-full p-5 bg-fundoClaro shadow-[0px_4px_4px_rgba(0,0,0,0.1)]">
+    <div className="w-full p-5 bg-rosa shadow-[0px_4px_4px_rgba(0,0,0,0.1)]">
       <div className="max-w-5xl mx-auto p-5">
-        <header className="text-center mb-10">
-          <Title1>Parceiros</Title1>
-        </header>
         <div className="flex flex-wrap justify-center gap-8 items-center">
           {partners.map((partner, index) => (
-            <Hexagon
-              size={hexSize}
-              key={index}
-              color={partner.color || "#020617"}
-            >
-              <a href={partner.link} className="flex items-center justify-center p-3">
+            <a key={index} className="flex items-center justify-center p-3">
                 <Image
                   src={partner.image}
                   alt={partner.nome}
                   width={120}
                   height={120}
-                  className={`object-contain w-40 h-40 ${partner.padding} rounded-lg`}
+                  className={`object-contain w-20 h-20 md:w-28 md:h-28 ${partner.padding} rounded-lg`}
                 />
               </a>
-            </Hexagon>
           ))}
-          <Hexagon size={hexSize}>
-            <div className="w-40 h-40 p-1 flex items-center text-center font-bold bg-[#36CAF4] rounded-lg border-2 border-azul">
-              <p>Participe você também!</p>
-            </div>
-          </Hexagon>
         </div>
       </div>
     </div>
