6a1585ce9b5892d6d763f27ebf5a7c994e300bc1
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2025-03-07 15:32:37 -0300

    atualizando processo

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -1,48 +1,46 @@
 import React from 'react';
 import Image from 'next/image';
 import Title1 from './text/Title1';
-import PrimaryButton from './button/PrimaryButton';
-import Card from './layout/Card';
+import Hexagon from './layout/Hexagon';
 
 export default function Process() {
     const process = [
         {
-            icon: "images/processo/processo1.svg",
+            icon: "images/processo/ideia.svg",
             title: "Geração de ideias",
             text: "Nesta fase, a equipe se reúne para brainstorm e discutir as necessidades do projeto. O foco é explorar ideias e definir os objetivos iniciais, identificando problemas a serem resolvidos e oportunidades a serem aproveitadas."
         },
         {
-            icon: "images/processo/processo2.svg",
+            icon: "images/processo/planejamento.svg",
             title: "Planejamento",
             text: "Na fase de planejamento, a equipe define as prioridades e cria um backlog inicial com as funcionalidades desejadas."
         },
         {
-            icon: "images/processo/processo3.svg",
+            icon: "images/processo/design.svg",
             title: "Design",
             text: "Durante a fase de design, são elaboradas as soluções técnicas e visuais do projeto."
         },
         {
-            icon: "images/processo/processo4.svg",
+            icon: "images/processo/desenvolvimento.svg",
             title: "Desenvolvimento",
             text: "Na fase de desenvolvimento, os programadores transformam o design em código."
         },
         {
-            icon: "images/processo/processo5.svg",
+            icon: "images/processo/teste.svg",
             title: "Teste e Avaliação",
             text: "Nesta fase, a equipe realiza testes rigorosos para identificar e corrigir bugs."
         },
         {
-            icon: "images/processo/processo6.svg",
+            icon: "images/processo/lançamento.svg",
             title: "Lançamento",
             text: "Na fase de lançamento, o produto é disponibilizado ao público."
         }
     ]
     return (
-        <div className="py-10 max-w-7xl min-h-[80vh] mx-auto flex flex-col justify-center gap-5 p-3">
+        <div className="max-w-7xl min-h-[80vh] mx-auto flex flex-col justify-center gap-5 p-3">
             {/* Coluna 1 */}
             <div className='relative'>
-                <Title1 className="mb-4 text-center">Processo</Title1>
-                <PrimaryButton className="w-full md:w-60 hidden">Nosso último projeto</PrimaryButton>
+                <Title1 className="mb-4 text-center text-[#54A6BD]">Processo</Title1>
                 <Image
                     src="/images/favos-de-mel.png"
                     width={100}
@@ -53,13 +51,13 @@ export default function Process() {
             </div>
 
             {/* Coluna 2 */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 m-auto md:m-0">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-8 m-auto md:m-0">
                 {process.map((item, index) => (
                     <div key={index} className="flex md:flex-col gap-3 text-center">
-                        <Card className='md:m-auto p-5'>
+                        <Hexagon className='md:m-auto p-5' color='#54A6BD'>
                             <Image src={item.icon} width={100} height={100} alt={`Ícone de ${item.title}`} className="w-10 h-10 row-span-1 md:row-span-2 md:mx-auto" />
-                        </Card>
-                        <h3 className="text-rosa font-bold text-lg my-auto">{index + 1} - {item.title}</h3>
+                        </Hexagon>
+                        <h3 className="text-azul font-bold text-lg my-auto">{index + 1} - {item.title}</h3>
                     </div>
                 ))}
             </div>
